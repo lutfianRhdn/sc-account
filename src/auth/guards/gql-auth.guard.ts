4f97ec84dc7cea75354d26d06b8983a422a7e8dd
FajarWG <fajarwahyugumelar@gmail.com>
2025-03-10 08:52:48 +0700

    feat: Add GraphQL authentication guard and update dependencies

---
+++ src/auth/guards/gql-auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class GqlAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
+  }
+
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      throw err || new UnauthorizedException('Unable to authenticate user');
+    }
+    return user;
+  }
+}
