61e3dfad16f099ad2b62f72462addbd89c58314f
Lutfian Rahdiansyah <lutfian.rhdn@gmail.com>
2025-08-19 12:13:57 +0700

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://application:K3p042412@cluster0.tn3np.mongodb.net/account')],
+  imports: [MongooseModule.forRoot('mongodb+srv://application:skripsi@socialabs.pjkgs8t.mongodb.net/account')],
 })
 export class DatabaseModule {}
